850a1db3349c823c8e40d6f6ccdf5e6400805075
Olivier Petit <petit-olivier@hotmail.fr>
2018-02-28 11:40:37 +0100

    Updated Wavelet block input/output strategy

--- Wavelet.cpp
+++ Wavelet.cpp
@@ -2,48 +2,52 @@
 
 Wavelet::Wavelet(sc_module_name name):sc_module(name)
 {
-	SC_METHOD(delay_even);
+	SC_METHOD(delay);
         sensitive << clk.pos() << reset;
-    SC_METHOD(delay_odd);
-    	sensitive << clk.pos() << reset;
+    
     SC_METHOD(compute_d)
     	sensitive << evenm2 << even << evenp2 << evenp4 << odd << reset;
+    
     SC_METHOD(compute_c)
-    	sensitive << even << dm1 << d_out << first_c << reset;
+    	sensitive << even << dm1 << d_int << first_c << reset;
+
+    SC_METHOD(output_select);
+    	sensitive << c_int << d_int << even << reset;
 }
 
-void Wavelet::delay_even()
+void Wavelet::delay()
 {
 	if(reset.read() == false)
 	{
+		// Even delay line
 		dm1.write(0);
 		evenm2.write(0);
 		even.write(0);
 		evenp2.write(0);
 		evenp4.write(0);
+
+		// Odd delay line
+		odd.write(0);
+		oddp2.write(0);
+		oddp4.write(0);
 	}
+	// Load new even pixel
 	else if(load_even.read() == true)
 	{
 		// Save old D value at this point
-		dm1.write(d_out.read());
+		dm1.write(d_int.read());
 
 		// Go 1 step forward
 		evenm2.write(even.read());
 		even.write(evenp2.read());
 		evenp2.write(evenp4.read());
 		evenp4.write(data_in.read());
-	}
-}
 
-void Wavelet::delay_odd()
-{
-	if(reset.read() == false)
-	{
-		odd.write(0);
-		oddp2.write(0);
-		oddp4.write(0);
+		// Save valid c value, so we can output it later
+		c_saved.write(c_int.read());
 	}
-	else if(load_odd.read() == true)
+	// Load new odd pixel
+	else if(load_even.read() == false)
 	{
 		odd.write(oddp2.read());
 		oddp2.write(oddp4.read());
@@ -55,11 +59,11 @@ void Wavelet::compute_d()
 {
 	if(reset.read() == false)
 	{
-		d_out.write(0);
+		d_int.write(0);
 	}
 	else
 	{
-		d_out.write(
+		d_int.write(
 			odd.read() 
 			- (9*(even.read() + evenp2.read()) 
 				- (evenm2.read() + evenp4.read())) / 16
@@ -72,17 +76,32 @@ void Wavelet::compute_c()
 {
 	if(reset.read() == false)
 	{
-		c_out.write(0);
+		c_int.write(0);
 	}
 	else
 	{
 		if(first_c.read() == true)
 		{
-			c_out.write(even.read() + d_out.read() / 2);
+			c_int.write(even.read() + d_int.read() / 2);
 		}
 		else
 		{
-			c_out.write(even.read() + (d_out.read() + dm1.read()) / 4);
+			c_int.write(even.read() + (d_int.read() + dm1.read()) / 4);
 		}
+	}
+}
+
+void Wavelet::output_select()
+{
+	if(reset.read() == false)
+	{
+		data_out.write(0);
+	}
+	else
+	{
+		if(even.read() == true)
+			data_out.write(d_int.read());
+		else
+			data_out.write(c_saved.read());
 	}
 }
--- Wavelet.h
+++ Wavelet.h
@@ -9,21 +9,23 @@ public:
    sc_in<bool> clk;
    sc_in<bool> reset;
    
+   // True if loading even pixel, false if loading odd pixel
    sc_in<bool> load_even;
-   sc_in<bool> load_odd;
+   // True when computing C_0
    sc_in<bool> first_c;
    sc_in<unsigned char> data_in;
-   
-   sc_out<unsigned char> c_out;
-   sc_out<unsigned char> d_out;
+
+   // Data out, alternatively C_n and D_n
+   sc_out<unsigned char> data_out;
 
    Wavelet(sc_module_name name);
 
-   void delay_even();
-   void delay_odd();
+   void delay();
 
    void compute_d();
    void compute_c();
+
+   void output_select();
 
    SC_HAS_PROCESS(Wavelet);
 
@@ -31,5 +33,16 @@ private:
    sc_signal<unsigned char> evenp4, evenp2, even, evenm2;
    sc_signal<unsigned char> oddp4, oddp2, odd;
    sc_signal<unsigned char> dm1;
+
+   // Combinationnal computation of c and d
+   // These are incorrect when we have loaded the even pixel but not the 
+   // following odd pixel
+   sc_signal<unsigned char> c_int;
+   sc_signal<unsigned char> d_int;
+
+   // Saves the last correct C value, so we can output it in between
+   // even pixel loading and odd pixel loading
+   sc_signal<unsigned char>  c_saved;
+
 };
 #endif
